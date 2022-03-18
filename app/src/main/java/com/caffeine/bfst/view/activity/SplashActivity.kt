30605a1f811b55e6059354ea5bc7ed8876c7ebb7
Saad Ahmed <saadahmedscse@gmail.com>
2022-03-18 20:33:04 +0600

    Check user in Splash Screen

--- app/src/main/java/com/caffeine/bfst/view/activity/SplashActivity.kt
+++ app/src/main/java/com/caffeine/bfst/view/activity/SplashActivity.kt
@@ -5,20 +5,57 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import androidx.activity.viewModels
+import androidx.navigation.Navigation
 import com.caffeine.bfst.R
+import com.caffeine.bfst.utils.AlertDialog
 import com.caffeine.bfst.utils.Constants
+import com.caffeine.bfst.utils.DataState
+import com.caffeine.bfst.view.fragment.AuthFragmentDirections
+import com.caffeine.bfst.viewmodel.AuthViewModel
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private val viewModel : AuthViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            if (Constants.currentUser != null){
-                Constants.intentToActivity(this, AuthenticationActivity::class.java)
+        if (Constants.auth.currentUser != null){
+            if (Constants.internetAvailable(this)){
+                viewModel.checkData()
             }
-            else Constants.intentToActivity(this, AuthenticationActivity::class.java)
-        }, 0)
+            else {
+                Constants.showSnackBar(this, window.decorView, "Check your internet connection", Constants.SNACK_LONG)
+            }
+        }
+
+        else {
+            Handler(Looper.getMainLooper()).postDelayed({
+                Constants.intentToActivity(this, AuthenticationActivity::class.java)
+            }, 1500)
+        }
+
+        viewModel.authLiveData.observe(this){
+            when (it) {
+                is DataState.Loading -> {}
+
+                is DataState.Success -> {
+                    if (it.data == "noInfo") {
+                        Constants.intentToActivity(this, AuthenticationActivity::class.java)
+                    }
+                    if (it.data == "hasInfo"){
+                        Constants.intentToActivity(this, HomeActivity::class.java)
+                    }
+                }
+
+                is DataState.Failed -> {
+                    Constants.showSnackBar(this, window.decorView, it.message!!, Constants.SNACK_LONG)
+                }
+            }
+        }
     }
 }
