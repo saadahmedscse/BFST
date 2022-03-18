57e86af85f7145d43ddb67f3d498cb6cdff9a6cb
Saad Ahmed <saadahmedscse@gmail.com>
2022-03-18 20:34:38 +0600

    User model class

---
+++ app/src/main/java/com/caffeine/bfst/services/model/UserDetails.kt
@@ -0,0 +1,11 @@
+package com.caffeine.bfst.services.model
+
+data class UserDetails(
+    val uid : String = "",
+    val name : String = "",
+    val number : String = "",
+    val bg : String = "",
+    val gender : String = "",
+    val dob : String = "",
+    val ldd : String = ""
+)
