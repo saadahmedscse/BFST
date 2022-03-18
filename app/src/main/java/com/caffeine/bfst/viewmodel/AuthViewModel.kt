4e687a2ae402c1abd0bf4d2bffe6be2ab3f154c2
Saad Ahmed <saadahmedscse@gmail.com>
2022-03-18 20:34:05 +0600

    Changed default mobile number

--- app/src/main/java/com/caffeine/bfst/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/caffeine/bfst/viewmodel/AuthViewModel.kt
@@ -5,21 +5,26 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.caffeine.bfst.services.repository.AuthRepository
+import com.caffeine.bfst.utils.Constants
 import com.caffeine.bfst.utils.DataState
 
 class AuthViewModel : ViewModel() {
 
     private val repository : AuthRepository = AuthRepository()
 
-    private val authMutableLiveData : MutableLiveData<DataState<String>> = MutableLiveData<DataState<String>>()
+    private val authMutableLiveData : MutableLiveData<DataState<String>> = Constants.getMutableDataStateOfString()
     val authLiveData : LiveData<DataState<String>>
         get() = authMutableLiveData
 
     fun sendVerificationCode(activity: Activity, number : String){
-        repository.sendOtpCode("+16505551234", activity, authMutableLiveData)
+        repository.sendOtpCode("+88$number", activity, authMutableLiveData)
     }
 
     fun verifyOTP(verificationID: String, code : String){
         repository.verifyCode(verificationID, code, authMutableLiveData)
+    }
+
+    fun checkData(){
+        repository.checkData(authMutableLiveData)
     }
 }
