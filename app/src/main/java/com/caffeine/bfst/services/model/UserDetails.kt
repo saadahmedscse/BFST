3b465125779979faa1d56aa70569f8fdae2f7855
Saad Ahmed <saadahmedscse@gmail.com>
2022-03-19 20:09:30 +0600

    Added location

--- app/src/main/java/com/caffeine/bfst/services/model/UserDetails.kt
+++ app/src/main/java/com/caffeine/bfst/services/model/UserDetails.kt
@@ -3,6 +3,7 @@ package com.caffeine.bfst.services.model
 data class UserDetails(
     val uid : String = "",
     val name : String = "",
+    val location : String = "",
     val number : String = "",
     val bg : String = "",
     val gender : String = "",
