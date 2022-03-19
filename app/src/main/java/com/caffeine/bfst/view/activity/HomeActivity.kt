7525d2e3c46b12bac7005fafc00e4163dececd2a
Saad Ahmed <saadahmedscse@gmail.com>
2022-03-19 20:10:36 +0600

    Added bottom navigation and nav host fragment

--- app/src/main/java/com/caffeine/bfst/view/activity/HomeActivity.kt
+++ app/src/main/java/com/caffeine/bfst/view/activity/HomeActivity.kt
@@ -2,11 +2,25 @@ package com.caffeine.bfst.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.caffeine.bfst.R
+import com.caffeine.bfst.databinding.ActivityHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.home_fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavView.setupWithNavController(navController)
     }
 }
