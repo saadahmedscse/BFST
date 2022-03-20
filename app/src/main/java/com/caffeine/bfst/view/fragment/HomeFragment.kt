c72c8a05d07205761053d694f5bb76d399b81bdb
Saad Ahmed <saadahmedscse@gmail.com>
2022-03-20 21:12:36 +0600

    Show post in recycler

--- app/src/main/java/com/caffeine/bfst/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/caffeine/bfst/view/fragment/HomeFragment.kt
@@ -5,15 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.caffeine.bfst.R
+import com.caffeine.bfst.databinding.FragmentHomeBinding
+import com.caffeine.bfst.utils.Constants
+import com.caffeine.bfst.utils.DataState
+import com.caffeine.bfst.view.adapter.PostAdapter
+import com.caffeine.bfst.viewmodel.PostViewModel
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding : FragmentHomeBinding
+    private val viewModel : PostViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+
+        binding.recyclerView.layoutManager = Constants.getVerticalLayoutManager(requireContext())
+        viewModel.getBloodPosts()
+
+        viewModel.postLiveData.observe(viewLifecycleOwner){
+            when (it){
+                is DataState.Loading -> {}
+
+                is DataState.Success -> {
+                    val adapter = PostAdapter(it.data!!, requireContext())
+                    binding.recyclerView.adapter = adapter
+                }
+
+                is DataState.Failed -> {}
+            }
+        }
+
+        return binding.root
     }
 }
